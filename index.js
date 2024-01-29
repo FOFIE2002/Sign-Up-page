86db15236fb0d9764939e0ab54a381f33c22dc1e
FOFIE2002 <fofiefopaelisabeth@gmail.com>
2024-01-29 11:28:22 +0100

    started styling the form

---
+++ index.js
@@ -0,0 +1,17 @@
+
+    $(document).ready(function () {
+        // Initialize the phone input with international format and dropdown
+        $("#phone").intlTelInput({
+            utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.12/js/utils.js",
+            separateDialCode: true,
+            initialCountry: "auto",
+            geoIpLookup: function (callback) {
+                // Use a GeoIP lookup service to determine the user's country
+                // You may want to replace this with a more reliable service
+                $.get("https://ipinfo.io", function () {}, "jsonp").always(function (resp) {
+                    var countryCode = (resp && resp.country) ? resp.country : "";
+                    callback(countryCode);
+                });
+            },
+        });
+    });
